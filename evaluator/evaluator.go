a7c91810e48ba07041c323b457a6b28a992db550
Olya Ivanova <olya@ivanovs.info>
2023-08-16 16:14:45 +1200

    Add buitin function len()

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -14,6 +14,23 @@ var (
 	NULL  = &object.Null{}
 )
 
+var builtins = map[string]*object.Builtin{
+	"len": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+		
+			switch arg := args[0].(type) {
+			case *object.String:
+				return &object.Integer{Value: int64(len(arg.Value))}
+			default:
+				return newError("argument to `len` not supported, got %s", args[0].Type())
+			}
+		},
+	},
+}
+
 
 func Eval(node ast.Node, env *object.Environment) object.Object {
 	switch node := node.(type) {
@@ -232,12 +249,15 @@ func evalStringLiteralInfixExpression(operator string, left, right object.Object
 }
 
 func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object {
-	val, ok := env.Get(node.Value)
-	if !ok {
-		return newError("identifier not found:" + node.Value)
+	if val, ok := env.Get(node.Value); ok {
+		return val
 	}
 	
-	return val
+	if buitin, ok := builtins[node.Value]; ok {
+		return buitin
+	}
+	
+	return newError("identifier not found:" + node.Value)
 }
 
 func evalMinusPrefixOperator(right object.Object) object.Object {
@@ -271,14 +291,16 @@ func evalBangOperator(right object.Object) object.Object {
 }
 
 func applyFunction(fn object.Object, args []object.Object) object.Object {
-	function, ok := fn.(*object.Function)
-	if !ok {
-		return newError("not a function: %s", fn.Type())
-	}
-	
-	extendedEnv := extendFunctionEnv(function, args)
-	evaluated := Eval(function.Body, extendedEnv)
-	return unwrapReturnValue(evaluated)
+	switch fn := fn.(type) {
+	case *object.Function:
+		extendedEnv := extendFunctionEnv(fn, args)
+		evaluated := Eval(fn.Body, extendedEnv)
+		return unwrapReturnValue(evaluated)
+	case *object.Builtin:
+		return fn.Fn(args...)
+	default:
+		return newError("not a function: %s", fn.Type())		
+	}	
 }
 
 func extendFunctionEnv(fn *object.Function, args []object.Object) *object.Environment {
--- object/object.go
+++ object/object.go
@@ -17,6 +17,7 @@ const (
 	ERROR_OBJ 		= "ERROR"
 	FUNCTION_OBJ	= "FUNCTION"
 	STRING_OBJ	    = "STRING"
+	BUILTIN_OBJ	    = "BUILTIN"
 )
 
 type Object interface {
@@ -120,3 +121,13 @@ func (f *Function) Inspect() string {
 	
 	return out.String()
 }
+
+// Built-in  function
+type BuiltinFunction func(args ...Object) Object
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string { return "builtin function" }
